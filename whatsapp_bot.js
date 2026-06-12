d81e060b9dfca6428ce374159c62552bb0efda94
dani0534346135-hub <dani0534346135@gmail.com>
2026-06-12 14:30:30 +0300

    Update whatsapp_bot.js

--- whatsapp_bot.js
+++ whatsapp_bot.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     if (latestQr) {
         res.send(`<h1>סרוק את הברקוד:</h1><img src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${encodeURIComponent(latestQr)}" />`);
     } else {
-        res.send('<h1>הבוט מחובר!</h1>');
+        res.send('<h1>הבוט מחובר ופעיל!</h1>');
     }
 });
 
@@ -27,17 +27,13 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
         puppeteer: {
             headless: true,
             args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-gpu',
-                '--single-process',
-                '--no-zygote'
+                '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage',
+                '--disable-gpu', '--single-process', '--no-zygote'
             ]
         }
     });
 
-    client.on('qr', (qr) => { latestQr = qr; console.log('QR מוכן!'); });
+    client.on('qr', (qr) => { latestQr = qr; console.log('QR מוכן לסריקה באתר'); });
     client.on('ready', () => { latestQr = ""; console.log('✅ הבוט מחובר!'); });
     client.initialize();
 });
